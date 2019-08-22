7124fc945c5544403449f5974186fbc19c2d062b
Glen Somerville <glen.somerville@gmail.com>
2019-08-22 16:49:10 +0300

    Removed redundant prod settings

--- hopechannelfi/settings/production.py
+++ hopechannelfi/settings/production.py
@@ -12,17 +12,6 @@ CSRF_COOKIE_DOMAIN = "hopechannel.fi"
 CSRF_COOKIE_SECURE = True
 CSRF_TRUSTED_ORIGINS = ['hopechannel.fi']
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'hopechannelfi',
-#         'USER': 'hopechannelfi_user',
-#         'PASSWORD': 'mypassword',
-#         'HOST': '127.0.0.1',
-#         'PORT': '5432',
-#     }
-# }
-
 try:
     from .local import *
 except ImportError:
