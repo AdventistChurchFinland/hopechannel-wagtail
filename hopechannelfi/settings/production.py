a989d9fdfe5035894db98bf1aaf17aeb141afd5d
Glen Somerville <glen.somerville@gmail.com>
2019-09-20 16:06:44 +0300

    Prod settings change

--- hopechannelfi/settings/production.py
+++ hopechannelfi/settings/production.py
@@ -8,9 +8,9 @@ SECURE_CONTENT_TYPE_NOSNIFF = True
 SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True
 
-CSRF_COOKIE_DOMAIN = "hopechannel.fi"
+CSRF_COOKIE_DOMAIN = "cms.hopechannel.fi"
 CSRF_COOKIE_SECURE = True
-CSRF_TRUSTED_ORIGINS = ['hopechannel.fi']
+CSRF_TRUSTED_ORIGINS = ['cms.hopechannel.fi']
 
 try:
     from .local import *
